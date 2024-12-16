713105fae4ab3b84714ea02f7e515496827f165d
mhortin20 <a02362215@aggies.usu.edu>
2024-12-16 15:57:48 -0700

    Orgonized and cleaned the student-table.

--- code/01_student-table.py
+++ code/01_student-table.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import warnings
-warnings.filterwarnings('ignore', category=pd.errors.SettingWithCopyWarning)
 
 # Data sheets
 student = pd.read_excel('data/2022 EOY Data - USU.xlsx', sheet_name='Student')
@@ -10,19 +8,17 @@ membership = pd.read_excel('data/2022 EOY Data - USU.xlsx', sheet_name='Course M
 courses = pd.read_excel('data/2022 EOY Data - USU.xlsx', sheet_name='Transcript Courses')
 
 
-# Rename 'StudentNumber' to 'student_number'
+# Rename 'StudentNumber' to 'student_number' in all tables that contain 'student_number'
 courses = courses.rename(columns={'StudentNumber': 'student_number'})
 membership = membership.rename(columns={'StudentNumber': 'student_number'})
 student = student.rename(columns={'StudentNumber': 'student_number'})
 
-# Create the df
-df = pd.DataFrame()
-
-# Create the model_df
-model_df = pd.DataFrame()
+#############################################################################
+# Filter at the end of all of the data wrangling?
 
 #------------------------------------------------------------------------------------------------------------------------------
 # Filter the student table down to 1 row per student. This will make everything easier moving forward
+# Drop columns that we do not need.
 student_columns_to_drop = [
     "ExitDate", "ResidentStatus", "KindergartenType", "EarlyGrad", "ReadGradeLevelFall", 
     "ReadGradeLevelSpring", "Biliteracy1Level", "Biliteracy1Language", "Biliteracy2Level",
@@ -30,48 +26,27 @@ student_columns_to_drop = [
     "EarlyNumeracyStatusEOY", "EarlyNumeracyIntervention"
 ]
 
-# Create the table student_table
-student_table = student
-
-# Drop the columns we do not want
-student_table = student_table.drop(columns = student_columns_to_drop)
-
-# Drop rows where student_number is null
-student_table = student_table.dropna(subset=['student_number'])
-student_table = student_table[student_table['student_number'] != '']
+# Filter student_table to one row per student (Remove rows with null or empty student_numbers and duplicate rows)
+student_table = student.drop(columns=student_columns_to_drop)
+student_table.dropna(subset=['student_number'], inplace=True)
 
 # Drop duplicate student_numbers but keep the row with the largest GradeLevel
-student_table = student_table.loc[
-    student_table.groupby('student_number')['GradeLevel'].idxmax()
-]
+student_table = student_table.loc[student_table.groupby('student_number')['GradeLevel'].idxmax()].reset_index(drop=True)
 
-# Reset index
-student_table.reset_index(drop=True, inplace=True)
-
-student_table.head()
-
-#############################################################################
-# Filter at the end of all of the data wrangling?
 
 #------------------------------------------------------------------------------------------------------------------------------
-# Start by building the df, and model_df with high school student_numbers from the student_table
-student_table['GradeLevel'] = pd.to_numeric(student['GradeLevel'], errors='coerce')
+# Create a table that contains all the columns as the student_table but only inlcudes student_numbers for high school students.
+# Ensure GradeLevel is numeric
+student_table['GradeLevel'] = pd.to_numeric(student_table['GradeLevel'], errors='coerce')
 
-# Filter the student table to include only high school students
-high_school_students = student_table[student_table['GradeLevel'] > 8][['student_number', 'GradeLevel']].copy()
-high_school_students['student_number'] = high_school_students['student_number'].astype(str)
+# Filter the student_table to only include student_numbers for students in grades > 8
+high_school_students = student_table[student_table['GradeLevel'] > 8].copy()
 
-# Create the df from the high_school_student student_numbers
-df = high_school_students[['student_number']]
+student_table.head()
+high_school_students.head()
 
-# Create the model_df from the high_school_student student_numbers
-model_df = high_school_students[['student_number']]
 
-df.head()
-
-#############################################################################
-# Export student_table.
-student_table.to_csv('./data/student_table.csv')
-
-# Export student_table.
-high_school_students.to_csv('./data/high_school_students.csv')
+#------------------------------------------------------------------------------------------------------------------------------
+# Export both tables
+student_table.to_csv('./data/student_table.csv', index=False)
+high_school_students.to_csv('./data/high_school_students.csv', index=False)

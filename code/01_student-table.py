f2482928be93b1fe68c7db25e5b329f2bf92c0d3
mhortin20 <a02362215@aggies.usu.edu>
2024-12-18 17:17:45 -0700

    Removed useless tables.

--- code/01_student-table.py
+++ code/01_student-table.py
@@ -3,20 +3,15 @@ import numpy as np
 
 # Data sheets
 student = pd.read_excel('data/2022 EOY Data - USU.xlsx', sheet_name='Student')
-master = pd.read_excel('data/2022 EOY Data - USU.xlsx', sheet_name='Course Master')
-membership = pd.read_excel('data/2022 EOY Data - USU.xlsx', sheet_name='Course Membership')
-courses = pd.read_excel('data/2022 EOY Data - USU.xlsx', sheet_name='Transcript Courses')
-
 
 # Rename 'StudentNumber' to 'student_number' in all tables that contain 'student_number'
-courses = courses.rename(columns={'StudentNumber': 'student_number'})
-membership = membership.rename(columns={'StudentNumber': 'student_number'})
 student = student.rename(columns={'StudentNumber': 'student_number'})
 
 #############################################################################
 # Filter at the end of all of the data wrangling?
+#############################################################################
 
-#------------------------------------------------------------------------------------------------------------------------------
+##########################################################################################################################################################
 # Filter the student table down to 1 row per student. This will make everything easier moving forward
 # Drop columns that we do not need.
 student_columns_to_drop = [
@@ -34,7 +29,7 @@ student_table.dropna(subset=['student_number'], inplace=True)
 student_table = student_table.loc[student_table.groupby('student_number')['GradeLevel'].idxmax()].reset_index(drop=True)
 
 
-#------------------------------------------------------------------------------------------------------------------------------
+##########################################################################################################################################################
 # Create a table that contains all the columns as the student_table but only inlcudes student_numbers for high school students.
 # Ensure GradeLevel is numeric
 student_table['GradeLevel'] = pd.to_numeric(student_table['GradeLevel'], errors='coerce')
@@ -46,7 +41,9 @@ student_table.head()
 high_school_students.head()
 
 
-#------------------------------------------------------------------------------------------------------------------------------
+##########################################################################################################################################################
 # Export both tables
 student_table.to_csv('./data/student_table.csv', index=False)
 high_school_students.to_csv('./data/high_school_students.csv', index=False)
+
+print("Data exported successfully!")

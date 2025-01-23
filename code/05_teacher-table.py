2c698a2017c9298806d83cb0c8c56dc5c45dc43d
mhortin20 <a02362215@aggies.usu.edu>
2025-01-22 19:40:01 -0700

    Updated the 05_teacher-table to include all years.

--- code/05_teacher-table.py
+++ code/05_teacher-table.py
@@ -1,72 +1,140 @@
+# The code will output two data files: 05_teacher_exploratory_data.csv and 05_teacher_modeling_data.csv
+
 import pandas as pd
-import numpy as np
-import warnings
-warnings.filterwarnings('ignore', category=pd.errors.SettingWithCopyWarning)
 
-# Load data sheets
-student = pd.read_excel('../data/2022 EOY Data - USU.xlsx', sheet_name='Student') 
-master = pd.read_excel('../data/2022 EOY Data - USU.xlsx', sheet_name='Course Master')
-membership = pd.read_excel('../data/2022 EOY Data - USU.xlsx', sheet_name='Course Membership')
-courses = pd.read_excel('../data/2022 EOY Data - USU.xlsx', sheet_name='Transcript Courses')
+# Define the years to process
+years = [2017, 2018, 2022, 2023, 2024]
 
-# Rename 'StudentNumber' to 'student_number' for consistency
-courses = courses.rename(columns={'StudentNumber': 'student_number'})
-membership = membership.rename(columns={'StudentNumber': 'student_number'})
-student = student.rename(columns={'StudentNumber': 'student_number'})
+# Specify the columns and their corresponding data types for each dataset to speed up the concatenation
+membership_columns = {'StudentNumber': 'int32', 'CourseRecordID': 'string'}
+master_columns = {'Teacher1ID': 'int32', 'CourseRecordID': 'string'}
+student_table_columns = {'student_number':'int32'}
+hs_students_columns = {'student_number': 'int32'}
 
-# Import student_table and high_school_student (for now)
-student_table = pd.read_csv('../data/student_table.csv')
-high_school_students = pd.read_csv('../data/high_school_students.csv')
+# Concatenate all years of data for each dataset into single DataFrames
+# This combines data from all years to create a complete record of the teachers had by students
 
+# Initialize empty lists to store year-specific DataFrames
+all_membership = []
+all_master = []
+all_student_tables = []
+all_hs_students = []
+
+# Loop through each year and load the data and drop duplicates immediatly to clean the data and speed up the loop
+for year in years:
+    membership_year = pd.read_excel(
+        f'data/{year} EOY Data - USU.xlsx', sheet_name='Course Membership', usecols=membership_columns.keys(), dtype=membership_columns
+        ).drop_duplicates()
+    
+    master_year = pd.read_excel(
+    f'data/{year} EOY Data - USU.xlsx', sheet_name='Course Master', usecols=master_columns.keys(),dtype=master_columns
+    ).drop_duplicates()
+
+    # low_memory=False removes the warning for mixed data types
+    student_table_year = pd.read_csv(
+        f'data/01_student_table_{year}.csv', usecols=student_table_columns.keys(), dtype=student_table_columns, low_memory=False
+        ).drop_duplicates()
+    
+    # low_memory=False removes the warning for mixed data types
+    hs_students_year = pd.read_csv(
+        f'data/01_high_school_students_{year}.csv', usecols=hs_students_columns.keys(), dtype=hs_students_columns, low_memory=False
+        ).drop_duplicates()
+
+    # Append year-specific data to the respective lists
+    all_membership.append(membership_year)
+    all_master.append(master_year)
+    all_student_tables.append(student_table_year[['student_number']])
+    all_hs_students.append(hs_students_year[['student_number']])
+
+
+######################################################################################################################################################
+# Concatenate and clean the DataFrames for each dataset across all years
+membership = pd.concat(all_membership, ignore_index=True)
+master = pd.concat(all_master, ignore_index=True)
+student_table = pd.concat(all_student_tables, ignore_index=True)
+hs_students = pd.concat(all_hs_students, ignore_index=True)
+
+# Rename column names in membership table
+membership = membership.rename(columns={'StudentNumber': 'student_number', 'CourseRecordID': 'course_record_id'})
+master = master.rename(columns={'Teacher1ID': 'teacher_id', 'CourseRecordID': 'course_record_id'})
+
+
+# Remove all duplicates from the dataframes after the data is concatinated
+membership = membership.drop_duplicates(keep='first')
+master = master.drop_duplicates(keep='first')
+student_table = student_table.drop_duplicates(keep='first')
+hs_students = hs_students.drop_duplicates(keep='first')
+
+
+######################################################################################################################################################
+# Merge the different tables to prepare for the pivot
+teacher_student = pd.merge(membership, master, on='course_record_id', how='left')
+
+####################################################################
+# If we decided to filter at the end, all we need to do is change hs_students to student_table
+# when creating the df below. The next line is the only line that needs to be adjusted.
+####################################################################
 # Create the df from the high_school_student student_numbers
-df = high_school_students[['student_number']]
+# - df: exploratory data
+# - model_df: model data
+df = hs_students[['student_number']].copy()
+model_df = hs_students[['student_number']].copy()
 
-# Create the model_df from the high_school_student student_numbers
-model_df = high_school_students[['student_number']]
+# Left join model_df and teacher_student so we only have student_numbers from the model_df
+# Only include the student_number and teacher_id from the teacher_student table
+teacher_student = pd.merge(model_df, teacher_student[['student_number', 'teacher_id']], on='student_number', how='left')
 
-###########################################################################
 
-#------------------------------------------------------------------------------------------------------------------------------
-# Student's Teacher by TeacherID and Grade for the model (which teachers students had and in what grade)
+######################################################################################################################################################
+# Pivot the merged table to create a grid of student_numbers by school_number
+teacher_grid = teacher_student.pivot_table(
+    index='student_number',
+    columns='teacher_id',
+    values='teacher_id',  # Use teacher_id to create binary attendance indicators
+    aggfunc=lambda x: 1,     # Set 1 to indicate a student has taken a class from a teacher
+    fill_value=0             # Fill missing values with 0 to indicate no attendance
+)
 
-course_data = courses[['student_number', 'GradeLevel']]
-membership_data = membership[['student_number', 'CourseRecordID']]
-master_data = master[['CourseRecordID', 'Teacher1ID']]
+# Rename the columns to make them more descriptive (i.e. school_[x])
+teacher_grid.columns = [f'teacher_{int(col)}' for col in teacher_grid.columns]
 
-# Merge master and membership on 'CourseRecordID' to get Teacher1ID per student
-teacher_membership = pd.merge(membership_data, master_data, on='CourseRecordID', how='left')
+# Reset the index to include student_number as a column for merging
+teacher_grid = teacher_grid.reset_index()
 
-# Merge with courses on 'student_number' to add GradeLevel
-teacher_data = pd.merge(teacher_membership, course_data, on='student_number', how='left')
+teacher_grid.head()
 
-# Drop rows that have the same combination of student_number, GradeLevel, and Teacher1ID
-# This removes exact duplicates while still allowing duplicate student_numbers to remain
-# The goal is to retain a list of all unique teachers that each student has had
-teacher_data = teacher_data.drop_duplicates()
+# Add a column to count the number of teachers each student had
+teacher_grid['teacher_count'] = teacher_grid.iloc[:, 1:].sum(axis=1)
 
-# Convert 'Teacher1ID' to strings
-teacher_data['Teacher1ID'] = teacher_data['Teacher1ID'].astype(str)
-
-# Create the 'Teacher_Grade' column
-teacher_data['Teacher_Grade'] = 'teacher_' + teacher_data['Teacher1ID']
-
-# Create dummy variables for 'Teacher_Grade'
-teacher_dummies = pd.get_dummies(teacher_data['Teacher_Grade'], prefix='', prefix_sep='')
-
-# Add 'student_number' back to the dummy DataFrame
-teacher_dummies['student_number'] = teacher_data['student_number']
-
-# Group by 'student_number' and sum the dummy variables to get the final DataFrame
-teacher_dummies = teacher_dummies.groupby('student_number').sum().reset_index()
-
-# Make sure student_number is a string
-teacher_dummies['student_number'] = teacher_dummies['student_number'].astype(str)
-model_df['student_number'] = model_df['student_number'].astype(str)
-
-# Add teacher_dummies to the model_df
-model_df = pd.merge(model_df, teacher_dummies, on='student_number', how='left')
-
-# Save the model_df to a CSV file in the data folder
-model_df.to_csv('../data/teacher_2022.csv', index=False)
+# Merge teacher_grid with the model_df
+model_df = pd.merge(model_df, teacher_grid, on='student_number', how='left')
 
 model_df.head()
+
+
+######################################################################################################################################################
+# Create a list of all the teachers a student has had for the exploratory data
+# This will create a column named teachers_had with a list of all the teachers a student has had for the exploratory data
+student_teacher_list = (
+    teacher_student.groupby('student_number')['teacher_id']
+    .apply(list)  # Aggregate school_number as a list
+    .reset_index()
+    .rename(columns={'teacher_id': 'teachers_had'})
+)
+
+# Merge the school_count column into the exploratory data (student_teacher_list)
+student_teacher_list = pd.merge(
+    student_teacher_list, teacher_grid[['student_number', 'teacher_count']], on='student_number', how='left')
+
+# Merge the student_school_list with the df
+df = pd.merge(df, student_teacher_list, on='student_number', how='left')
+
+df.head()
+
+
+######################################################################################################################################################
+# Export the data
+df.to_csv('./data/05_teacher_exploratory_data.csv', index=False)
+model_df.to_csv('./data/05_teacher_modeling_data.csv', index=False)
+
+print('Teacher data exported successfully!')

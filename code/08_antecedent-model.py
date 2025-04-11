cd83369c96c0f24666be889edd92fa7c37059c09
Marc Dotson <marc.dotson@usu.edu>
2025-04-10 21:51:55 -0600

    Specifying model runs with indicators

--- code/08_antecedent-model.py
+++ code/08_antecedent-model.py
@@ -1,89 +1,29 @@
 import pandas as pd
 import bambi as bmb
-import arviz as az
-# import networkx as nx
 import os
-import matplotlib.pyplot as plt
 import glob
 
-# ################################################
-# #CREATE A DAG TO JUSTIFY THE MODEL
-# ################################################
-# G = nx.DiGraph()
 
-# # Add edges based on the DAG structure
-# edges = [
-#     ("Teacher", "AC_Ind"),
-#     ("Teacher", "Overall_GPA"),
-#     ("Attendance", "Overall_GPA"),
-#     ("School", "Teacher"),
-#     ("School", "Overall_GPA"),
-#     ("School", "AC_Ind"),
-#     ("Disadvantage", "Overall_GPA"),
-#     ("Disadvantage", "Attendance"),
-#     ("Disadvantage", "AC_Ind"),
-#     ("Overall_GPA", "AC_Ind")
-# ]
+########################################################
+# SPECIFY WHAT MODEL TO RUN
+########################################################
 
-# G.add_edges_from(edges)
-
-# # Draw the DAG
-# plt.figure(figsize=(10, 8))
-# pos = nx.shell_layout(G)  # Trying a shell layout for better separation
-# nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=3000, font_size=10)
-# plt.title("DAG for Advanced Coursework ASC Project")
-# plt.show()
+data_model_ind = 1          # Use the entire modeling data
+data_post_covid_ind = 0     # Use the post-covid modeling data
+group_high_school_ind = 1   # Group by high schools
+group_middle_school_ind = 0 # Group by middle schools
 
 
 ########################################################
 # LOAD IN THE DATASET AND ESTABLISH FOLDER PATH
 ########################################################
 
-df = pd.read_csv('data/modeling_data.csv', low_memory = False)
-# df = pd.read_csv('data/post_covid_modeling_data.csv', low_memory = False)
+# Load in the data based on the indicators
+if data_model_ind == 1:
+    df = pd.read_csv('data/modeling_data.csv', low_memory = False)
 
-
-#######################################################
-# import polars as pl
-
-# df_pl = pl.DataFrame(df)
-
-# df_pl.shape
-# df_pl.columns
-
-# df_pl.select(pl.col('student_number')).shape
-# df_pl.select(pl.col('student_number')).unique().shape
-
-# (df_pl
-#     .group_by(pl.col('extracurricular_ind'))
-#     .agg(n = pl.len())
-# )
-
-# (df_pl
-#     .group_by(pl.col('high_school'))
-#     .agg(n = pl.len())
-# )
-
-# (df_pl
-#     .group_by(pl.col('middle_school'))
-#     .agg(n = pl.len())
-# )
-
-# # # Automatically identify all binary columns (True/False or 0/1)
-# # binary_cols = [
-# #     col for col, dtype in zip(df_pl.columns, df_pl.dtypes)
-# #     if (dtype in [pl.Boolean, pl.Int8, pl.Int16, pl.Int32, pl.Int64]) and col != 'ell_disability_group'
-# # ]
-
-# # # Compute counts of binary columns grouped by categorical variable
-# # counts = (df_pl
-# #     .group_by(pl.col('ell_disability_group'))
-# #     .agg([
-# #         pl.col(bin_col).sum().alias(f"{bin_col}_count") for bin_col in binary_cols
-# #     ])
-# #     .sort(pl.col('ell_disability_group'))
-# # )
-#######################################################
+if data_post_covid_ind == 1:
+    df = pd.read_csv('data/post_covid_modeling_data.csv', low_memory = False)
 
 # Define the folder path where the model output will be saved
 folder_path = "output/"
@@ -97,86 +37,45 @@ if not os.path.exists(folder_path):
 # PREP THE MODEL AND SPECIFY THE MODEL FORMULA
 #######################################################
 
-# # Columns to exclude from modeling for model_data
-# col_drop = ['student_number']
-# for col in df.columns:
-#     if col.startswith('teacher') or col.startswith('exit'):
-#         col_drop.append(col)
+# Columns to exclude from modeling for model_data
+if data_model_ind == 1:
+    col_drop = ['student_number', 'hs_advanced_math_y']
+    for col in df.columns:
+        if col.startswith('teacher') or col.startswith('exit'):
+            col_drop.append(col)
 
 # Columns to exclude from modeling for post_covid_modeling_data
-col_drop = ['student_number', 'hs_advanced_math_y']
-for col in df.columns:
-    if col.startswith('teacher') or col.startswith('exit') or col.startswith('envi') or col.startswith('tribal_affiliation_g'):
-        col_drop.append(col)
+if data_post_covid_ind == 1:
+    col_drop = ['student_number', 'hs_advanced_math_y', 'tribal_affiliation_g']
+    for col in df.columns:
+        if col.startswith('teacher') or col.startswith('exit') or col.startswith('envi'):
+            col_drop.append(col)
 
-#filter students out from cache high and who have no high school assigned
-df = df[~df['high_school'].isin(['Cache High', '0'])]
+# Filter students out from Cache High who have no high school assigned
+if group_high_school_ind == 1:
+    df = df[~df['high_school'].isin(['Cache High', '0'])]
 
-#filter students who have no middle school assigned
-df = df[~df['middle_school'].isin(['0'])]
+# Filter students who have no middle school assigned and drop more columns
+if group_middle_school_ind == 1:
+    df = df[~df['middle_school'].isin(['0'])]
 
-# Define base dataframe after dropping columns
-df_base = df.drop(columns=col_drop, axis=1)
+    more_col_drop = ['gender_u', 'migrant_y', 'military_child_y', 
+        'refugee_student_y', 'homeless_y', 'part_time_home_school_y', 
+        'immigrant_y', 'tribal_affiliation_n', 'tribal_affiliation_p', 
+        'tribal_affiliation_s', 'tribal_affiliation_u']
 
-# # # create a list of columns to include as random effects
-# # rand_effects = []
-# # for col in df_base.columns:
-# #     if col.startswith('school'): # Just include schools as random effects
-# #         rand_effects.append(col)
-# fixed_effects = []
-# for col in df_base.columns:
-#     if col.startswith('school'): # Just include schools as random effects
-#         fixed_effects.append(col)
+    col_drop = col_drop + more_col_drop
 
-# rand_effects = " + ".join(df_base.columns.difference(["ac_ind", "high_school", "middle_school"]))
-
-# # Includes zero counts!
-# zero_counts = ['migrant_y', 'military_child_y', 'refugee_student_y', 'homeless_y',
-#     'part_time_home_school_y', 'services_504_y', 'immigrant_y',
-#     'tribal_affiliation_n', 'tribal_affiliation_p']
-
-# Extract potential predictors (excluding the target 'ac_ind')
-# all_predictors = " + ".join(df_base.columns.difference(["ac_ind", "ell_disability_group"]))
+# Define base data frame after dropping columns and specify predictors
+df_base = df.drop(columns = col_drop, axis=1)
 all_predictors = " + ".join(df_base.columns.difference(["ac_ind", "high_school", "middle_school"]))
 
-# # Include secondary schools as fixed effects.
-# df_high_schools = pd.get_dummies(df_base['high_school'], dtype = float).rename(columns = {'0': 'Unknown_High', 'Cache High': 'Cache_High', 'Green Canyon': 'Green_Canyon', 'Mountain Crest': 'Mountain_Crest', 'Ridgeline': 'Ridgeline', 'Sky View': 'Sky_View'})
-# df_middle_schools = pd.get_dummies(df_base['middle_school'], dtype = float).rename(columns = {'0': 'Unknown_Middle', 'North Cache Middle': 'North_Cache_Middle', 'South Cache Middle': 'South_Cache_Middle', 'Spring Creek Middle': 'Spring_Creek_Middle'})
-# df_base = pd.concat([df_base, df_high_schools, df_middle_schools], axis = 1)
-# # df_base = pd.concat([df_base, df_high_schools], axis = 1)
+# Specify the model formula
+if group_high_school_ind == 1:
+    model_formula = f"ac_ind ~ ({all_predictors} | high_school)"
 
-# fixed_effects = []
-# for col in df_high_schools.columns.difference(['Unknown_High']):
-#     fixed_effects.append(col)
-# for col in df_middle_schools.columns.difference(['Unknown_Middle']):
-#     fixed_effects.append(col)
-
-# fixed_effects = " + ".join(fixed_effects)
-
-# # fixed_effects = (" + ".join(df_base.columns
-# #     .difference(["ac_ind", "ell_disability_group"])
-# #     .difference(rand_effects))
-# # )
-# rand_effects = (" + ".join(df_base.columns
-#     .difference(["ac_ind", "ell_disability_group"])
-#     # .difference(fixed_effects))
-#     .difference(fixed_effects)
-#     .difference(zero_counts))
-# )
-
-# # rand_effects = " + ".join(rand_effects)
-# fixed_effects_01 = " + ".join(fixed_effects)
-# fixed_effects_02 = " + ".join(zero_counts)
-
-#create the string formated model formula 
-# model_formula = f"ac_ind ~ {all_predictors} + ({all_predictors} | ell_disability_group)"
-
-# model_formula = f"ac_ind ~ ({all_predictors} | high_school)"
-model_formula = f"ac_ind ~ ({all_predictors} | middle_school) + ({all_predictors} | high_school)"
-
-# model_formula = f"ac_ind ~ {fixed_effects} + ({rand_effects} | high_school)"
-# model_formula = f"ac_ind ~ {fixed_effects} + ({rand_effects} | ell_disability_group)"
-# model_formula = f"ac_ind ~ {fixed_effects_01} + {fixed_effects_02} + ({rand_effects} | ell_disability_group)"
+if group_middle_school_ind == 1:
+    model_formula = f"ac_ind ~ ({all_predictors} | middle_school) + ({all_predictors} | high_school)"
 
 
 ################################################
@@ -186,26 +85,27 @@ model_formula = f"ac_ind ~ ({all_predictors} | middle_school) + ({all_predictors
 if __name__ == '__main__':
     print("Starting model setup...")
 
-    #establish the flat Logistic Regression Model
+    # Establish the Multilevel Logistic Regression Model
     multilevel_model = bmb.Model(model_formula, df_base, family = "bernoulli", noncentered = True)
-    # apply the .build() method prior to fitting the model
+    
+    # Apply the .build() method prior to fitting the model
     multilevel_model.build() 
 
-    # Run the sampling *******ADJUST THE AMOUNT OF SAMPLING (TUNE, DRAW, ETC.) HERE AS NEEDED ********
+    # Run the sampling (ADJUST THE AMOUNT OF SAMPLING (TUNE, DRAW, ETC.) HERE AS NEEDED)
     try:
         print("Starting model sampling...")
-        #fit the model with the the flat_model that was created prior
-        # multilevel_fitted = multilevel_model.fit(
-        #     draws=2000, inference_method='mcmc', random_seed=42, target_accept = .9, 
-        #     idata_kwargs={"log_likelihood": True})
-        # multilevel_fitted = multilevel_model.fit(idata_kwargs = {"log_likelihood": True})
-        multilevel_fitted = multilevel_model.fit(draws=2000, idata_kwargs = {"log_likelihood": True})
+        
+        if group_high_school_ind == 1:
+            multilevel_fitted = multilevel_model.fit(idata_kwargs = {"log_likelihood": True})
+        
+        if group_middle_school_ind == 1:
+            multilevel_fitted = multilevel_model.fit(draws=2000, idata_kwargs = {"log_likelihood": True})
+        
         print("Sampling complete.")
 
     except Exception as e:
         print(f"Sampling failed: {e}")
         multilevel_fitted = None  # Set output to None if sampling fails
-
 
 
 ###############################################################

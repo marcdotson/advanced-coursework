86ce0ab4c80fd6f649e31e518e591ceb0caac667
Marc Dotson <marc.dotson@usu.edu>
2025-04-15 19:42:33 -0600

    Cleaned up conditional statements for antecedent model

--- code/08_antecedent-model.py
+++ code/08_antecedent-model.py
@@ -9,10 +9,8 @@ import glob
 # SPECIFY WHAT MODEL TO RUN
 ########################################################
 
-data_model_ind = 1          # Use the entire modeling data
-data_post_covid_ind = 0     # Use the post-covid modeling data
-group_high_school_ind = 1   # Group by high schools
-group_middle_school_ind = 0 # Group by middle schools
+post_covid_data_ind = 1  # Only use the post-covid data
+multilevel_model_ind = 0 # Run a multilevel model
 
 
 ########################################################
@@ -20,11 +18,10 @@ group_middle_school_ind = 0 # Group by middle schools
 ########################################################
 
 # Load in the data based on the indicators
-if data_model_ind == 1:
-    df = pd.read_csv('data/modeling_data.csv', low_memory = False)
-
-if data_post_covid_ind == 1:
+if post_covid_data_ind == 1:
     df = pd.read_csv('data/post_covid_modeling_data.csv', low_memory = False)
+else:
+    df = pd.read_csv('data/modeling_data.csv', low_memory = False)
 
 # Define the folder path where the model output will be saved
 folder_path = "output/"
@@ -45,48 +42,16 @@ for col in df.columns:
         col_drop.append(col)
 
 # Filter students out from Cache High who have no high school assigned
-# if group_high_school_ind == 1:
 df = df[~df['high_school'].isin(['Cache High', '0'])]
-
-# # Filter students who have no middle school assigned and drop more columns
-# if group_middle_school_ind == 1:
-#     df = df[~df['middle_school'].isin(['0'])]
-# 
-#     more_col_drop = ['gender_u', 'migrant_y', 'military_child_y', 
-#         'refugee_student_y', 'homeless_y', 'part_time_home_school_y', 
-#         'immigrant_y', 'tribal_affiliation_n', 'tribal_affiliation_p', 
-#         'tribal_affiliation_s', 'tribal_affiliation_u']
-# 
-#     col_drop = col_drop + more_col_drop
 
 # Define base data frame after dropping columns and specify predictors
 df_base = df.drop(columns = col_drop, axis=1)
-# all_predictors = " + ".join(df_base.columns.difference(["ac_ind", "high_school", "middle_school"]))
 
 # Specify the model formula
-if group_high_school_ind == 1:
-    # all_predictors = " + ".join(df_base.columns.difference(["ac_ind", "high_school", "middle_school"]))
+if multilevel_model_ind == 1:
     all_predictors = " + ".join(df_base.columns.difference(["ac_ind", "high_school"]))
     model_formula = f"ac_ind ~ ({all_predictors} | high_school)"
-    # model_formula = f"ac_ind ~ {all_predictors} + ({all_predictors} | high_school)"
-
-elif group_middle_school_ind == 1:
-    all_predictors = " + ".join(df_base.columns.difference(["ac_ind", "high_school", "middle_school"]))
-    model_formula = f"ac_ind ~ ({all_predictors} | middle_school) + ({all_predictors} | high_school)"
-
 else:
-    # df_base = pd.get_dummies(df_base, columns=['high_school'], drop_first=False, dtype=int)
-    # df_base = pd.get_dummies(df_base, columns=['middle_school'], drop_first=True, dtype=int)
-
-    # df_base = df_base.rename(columns={
-    #     'high_school_Green Canyon': 'high_school_Green_Canyon',
-    #     'high_school_Mountain Crest': 'high_school_Mountain_Crest',
-    #     'high_school_Sky View': 'high_school_Sky_View',
-    #     'middle_school_North Cache Middle': 'middle_school_North_Cache_Middle',
-    #     'middle_school_South Cache Middle': 'middle_school_South_Cache_Middle',
-    #     'middle_school_Spring Creek Middle': 'middle_school_Spring_Creek_Middle'
-    # })
-
     all_predictors = " + ".join(df_base.columns.difference(["ac_ind"]))
     model_formula = f"ac_ind ~ {all_predictors}"
 
@@ -97,35 +62,19 @@ else:
 if __name__ == '__main__':
     print("Starting model setup...")
 
-    if group_high_school_ind == 1 or group_middle_school_ind == 1:
-        # Establish the Multilevel Logistic Regression Model
+    if multilevel_model_ind == 1:
         multilevel_model = bmb.Model(model_formula, df_base, family = "bernoulli", noncentered = True)
-        
-        # Apply the .build() method prior to fitting the model
         multilevel_model.build()
-
     else:
-        # Establish the Flat Logistic Regression Model
         flat_model = bmb.Model(model_formula, df_base, family = "bernoulli")
-        
-        # Apply the .build() method prior to fitting the model
         flat_model.build()
 
     # Run the sampling (ADJUST THE AMOUNT OF SAMPLING (TUNE, DRAW, ETC.) HERE AS NEEDED)
     try:
         print("Starting model sampling...")
         
-        # if group_high_school_ind == 1:
-        #     multilevel_fitted = multilevel_model.fit(idata_kwargs = {"log_likelihood": True})
-        #     # multilevel_fitted = multilevel_model.fit(draws=2000, idata_kwargs = {"log_likelihood": True})
-        
-        # if group_middle_school_ind == 1:
-        #     multilevel_fitted = multilevel_model.fit(draws=2000, idata_kwargs = {"log_likelihood": True})
-        
-        if group_high_school_ind == 1 or group_middle_school_ind == 1:
-            # multilevel_fitted = multilevel_model.fit(idata_kwargs = {"log_likelihood": True})
+        if multilevel_model_ind == 1:
             multilevel_fitted = multilevel_model.fit(draws=2000, idata_kwargs = {"log_likelihood": True})
-
         else:
             flat_fitted = flat_model.fit(idata_kwargs = {"log_likelihood": True})
 
@@ -134,11 +83,10 @@ if __name__ == '__main__':
     except Exception as e:
         print(f"Sampling failed: {e}")
 
-        if group_high_school_ind == 1 or group_middle_school_ind == 1:
-            multilevel_fitted = None  # Set output to None if sampling fails
-
+        if multilevel_model_ind == 1:
+            multilevel_fitted = None
         else:
-            flat_fitted = None        # Set output to None if sampling fails
+            flat_fitted = None
 
 
 ###############################################################
@@ -156,9 +104,8 @@ def get_next_filename(folder_path, base_name, extension):
     next_number = existing_numbers[-1] + 1 if existing_numbers else 1
     return f"{folder_path}/{base_name}_{next_number:02d}.{extension}"
 
-
 # Only try to export if the model is not None
-if (group_high_school_ind == 1 or group_middle_school_ind == 1) and multilevel_fitted is not None:
+if multilevel_model_ind == 1 and multilevel_fitted is not None:
     print('Saving Model Output to a File...')
 
     # Generate incremented filenames
@@ -179,7 +126,7 @@ if (group_high_school_ind == 1 or group_middle_school_ind == 1) and multilevel_f
     sorted_summary.to_csv(csv_filename)
     print(f"Ordered model output saved as {csv_filename}!")
 
-if group_high_school_ind == 0 and group_middle_school_ind == 0 and flat_fitted is not None:
+elif multilevel_model_ind == 0 and flat_fitted is not None:
     print('Saving Model Output to a File...')
 
     # Generate incremented filenames
@@ -221,5 +168,9 @@ else:
 # 09 - 08 run on the post-COVID data.
 # 10 - 08 as a mixed effect model, including all predictors as both fixed and random effects.
 # 11 - 10 run for twice as long.
-# 12 - 08 but including middle_school as a fixed effect.
+# 12 - 08 but including middle_school as a random effect.
+
+# Final Models:
+# Full Dataset: Flat Model 06 + Multilevel Model 12.
+# Post-COVID: Flat Model 07 + Multilevel Model 13.
 
